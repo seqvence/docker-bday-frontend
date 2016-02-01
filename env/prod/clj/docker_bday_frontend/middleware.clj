683ca41726176eaa70832a068d67cec71149374d
alex giurgiu <alex.giurgiu@gmail.com>
2016-02-02 00:20:46 +0100

    switched to the figwheel template

--- env/prod/clj/docker_bday_frontend/middleware.clj
+++
@@ -1,5 +0,0 @@
-(ns docker-bday-frontend.middleware
-  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
-
-(defn wrap-middleware [handler]
-  (wrap-defaults handler site-defaults))
--- env/prod/cljs/docker_bday_frontend/prod.cljs
+++
@@ -1,7 +0,0 @@
-(ns docker-bday-frontend.prod
-  (:require [docker-bday-frontend.core :as core]))
-
-;;ignore println statements in prod
-(set! *print-fn* (fn [& _]))
-
-(core/init!)
