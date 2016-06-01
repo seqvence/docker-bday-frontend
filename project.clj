16a52289ea4d7a579959bb7f65e398bb0a6af53c
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2016-06-01 12:03:42 +0200

    cleaned up config

--- project.clj
+++ project.clj
@@ -34,7 +34,6 @@
                {:id "dev"
                 :source-paths ["src"  "checkouts/gmaps-cljs/src"]
 
-                ;; If no code is to be run, set :figwheel true for continued automagical reloading
                 :figwheel {:on-jsload "docker-bday-frontend.core/on-js-reload"}
 
                 :compiler {:main docker-bday-frontend.core
@@ -46,10 +45,6 @@
                                            :provides ["markerclusterer"]}]
                            :source-map-timestamp true}}
 
-               ; This next build is an compressed minified build for
-               ; production. You can build this with:
-               ; lein cljsbuild once min
-
                {:id "min"
                 :source-paths ["src" "checkouts/gmaps-cljs/src"]
                 :compiler {:output-to "resources/public/js/compiled/docker_bday_frontend.min.js"
@@ -58,33 +53,4 @@
                            :pretty-print false}}
                ]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             })
+  :figwheel {:css-dirs ["resources/public/css"]})
